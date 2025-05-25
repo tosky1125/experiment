307fac65530347b65a5c3adb4a6ba315cf1e7946
Shawn <65295172+tosky1125@users.noreply.github.com>
2025-05-25 23:51:04 +0200

    implemented singleton pattered service test

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -26,5 +28,14 @@ public class SingletonTest {
         SingletonService singletonService2 = SingletonService.getInstance();
         assertThat(singletonService).isInstanceOf(SingletonService.class);
         assertThat(singletonService).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("Spring Container & Singleton")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isSameAs(memberService2);
     }
 }
